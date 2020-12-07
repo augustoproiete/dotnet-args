5e138170612482bac637ce3284e9baf91c45495b
C. Augusto Proiete <augusto@proiete.com>
2020-12-06 20:21:48 -0400

    Initial version of dotnet-args

---
+++ README.md
@@ -0,0 +1,62 @@
+| README.md |
+|:---|
+
+<div align="center">
+
+![dotnet-args](asset/dotnet-args-logo.png)
+
+</div>
+
+<h1 align="center">dotnet-args</h1>
+<div align="center">
+
+ Cross-platform .NET CLI Tool that prints the command-line arguments it receives, to help debug scripts and apps.
+
+[![NuGet Version](http://img.shields.io/nuget/v/dotnet-args.svg?style=flat-square)](https://www.nuget.org/packages/dotnet-args/) [![.NET](https://img.shields.io/badge/.NET%20-%3E%3D%205.0-informational)](https://dotnet.microsoft.com/download) [![.NET Core](https://img.shields.io/badge/.NET%20Core-%3E%3D%202.1-informational)](https://dotnet.microsoft.com/download) [![Stack Overflow](https://img.shields.io/badge/stack%20overflow-dotnet--tool-orange.svg?style=flat-square)](http://stackoverflow.com/questions/tagged/dotnet-tool)
+
+![Screenshot of dotnet-args in action](asset/dotnet-args-screenshot.png)
+
+</div>
+
+## Give a Star! :star:
+
+If you like or are using this project please give it a star. Thanks!
+
+## Background
+
+When developing applications that can receive command-line arguments, it can be useful to test what command-line arguments the application would receive without actually running the application.
+
+Another example where `dotnet-args` can be useful is when writing scripts for multiple platforms (e.g. `run.cmd`, `run.ps1`, `run.sh`, etc.) that _forward_ arguments to an application or another script. You can modify the scripts temporarily to forward the arguments to `dotnet-args` instead of the real application, and test that the scripts are forwading the arguments as expected.
+
+## Prerequisites
+
+**dotnet-args** runs on Windows, OSX, and Linux and requires one of the following versions of .NET:
+- .NET 5.0 or higher
+- .NET Core 3.1 or higher
+- .NET Core 2.1 or higher
+
+## Getting started :rocket:
+
+Install the `dotnet-args` .NET CLI Tool from NuGet:
+
+```powershell
+dotnet tool install dotnet-args --global
+```
+
+Next, call `dotnet-args` with the arguments that you'd like to test, for example:
+
+```powershell
+dotnet args --file project.zip --message "My project!"
+```
+
+![Screenshot of dotnet-args in action](asset/dotnet-args-screenshot.png)
+
+> More details on .NET CLI tools available on the official [documentation](https://docs.microsoft.com/en-us/dotnet/core/tools/global-tools) from Microsoft.
+
+## Release History
+
+Click on the [Releases](https://github.com/augustoproiete/dotnet-args/releases) tab on GitHub.
+
+---
+
+_Copyright &copy; 2018-2020 C. Augusto Proiete & Contributors - Provided under the [Apache License, Version 2.0](LICENSE)._
---
+++ src/DotNet.Args/Program.cs
@@ -0,0 +1,86 @@
+﻿// Copyright 2020 C. Augusto Proiete & Contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+
+namespace DotNet.Args
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            try
+            {
+                var appVersion = typeof(Program).Assembly.GetCustomAttributes(true)
+                    .OfType<AssemblyInformationalVersionAttribute>().Single().InformationalVersion;
+
+                var appTitle = $"dotnet-args CLI {appVersion}";
+                Console.WriteLine(appTitle, ConsoleColor.White);
+
+                Print(args);
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message, ConsoleColor.Red);
+                Console.WriteLine();
+                Console.WriteLine(ex.ToString());
+                return 1;
+            }
+        }
+
+        private static void Print(string[] args)
+        {
+            if (args is null || args.Length == 0)
+            {
+                Console.WriteLine("Usage: dotnet args <Argument1> [Argument2] [Argument3] [...]");
+                Console.WriteLine("Example: dotnet args --file project.zip --message \"My project!\"", ConsoleColor.DarkGray);
+                Console.WriteLine();
+                Console.WriteLine("No command-line arguments received.", ConsoleColor.Red);
+                return;
+            }
+
+            Console.Write("Command-line arguments received (");
+            Console.Write(args.Length.ToString(CultureInfo.InvariantCulture), ConsoleColor.Magenta);
+            Console.Write("):");
+            Console.WriteLine();
+
+            var argIndex = 0;
+            while (argIndex < args.Length)
+            {
+                Console.Write("- args[");
+                Console.Write(argIndex.ToString(CultureInfo.InvariantCulture), ConsoleColor.Yellow);
+                Console.Write("]: ");
+                Console.Write(FormattableString.Invariant($"{args[argIndex]}"), ConsoleColor.Cyan);
+                Console.WriteLine();
+                argIndex++;
+            }
+
+            var firstArg = Environment.GetCommandLineArgs()[0];
+            var commandLine = Environment.CommandLine;
+
+            if (!string.IsNullOrWhiteSpace(firstArg) && firstArg.EndsWith("dotnet-args.dll", StringComparison.OrdinalIgnoreCase))
+            {
+                commandLine = commandLine.Remove(0, firstArg.Length).TrimStart();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Complete command-line:");
+            Console.WriteLine(commandLine);
+        }
+    }
+}
